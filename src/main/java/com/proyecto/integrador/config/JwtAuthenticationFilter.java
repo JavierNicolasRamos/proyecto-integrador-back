0a3da3911a3618ed6f8cbad1873ee3b8df31bfc9
Bipperty <ignacio.gibbs@gmail.com>
2023-10-31 19:52:42 -0300

    Adding JwtService

--- src/main/java/com/proyecto/integrador/config/JwtAuthenticationFilter.java
+++ src/main/java/com/proyecto/integrador/config/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package com.proyecto.integrador.config;
 
+import com.proyecto.integrador.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +15,9 @@ import java.io.IOException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
     @Override
     protected void doFilterInternal(
             @NonNull HttpServletRequest request,
@@ -21,5 +25,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
 
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
     }
 }
