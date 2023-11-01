2c3c1bf57787a83a1ab4a322e7bf2a4e7b09edd8
Bipperty <ignacio.gibbs@gmail.com>
2023-11-01 18:53:20 -0300

    DaoAuthProvider & passwordEncoder

--- src/main/java/com/proyecto/integrador/config/ApplicationConfig.java
+++ src/main/java/com/proyecto/integrador/config/ApplicationConfig.java
@@ -4,9 +4,13 @@ import com.proyecto.integrador.repository.AuthRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,5 +23,19 @@ public class ApplicationConfig {
         return username -> authRepository.findByEmail(username)
                 .map(UserDetails.class::cast)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Bean
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
