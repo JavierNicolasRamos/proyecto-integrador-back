39e2c1f1adae6d4cc310a65b749102f6f90bbcd1
Bipperty <ignacio.gibbs@gmail.com>
2023-11-01 20:00:34 -0300

    adding requests & responses

---
+++ src/main/java/com/proyecto/integrador/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package com.proyecto.integrador.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+}
