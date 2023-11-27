4fc38dba9ef766939cec0353c7f02dbed376f2d8
Bipperty <ignacio.gibbs@gmail.com>
2023-11-27 19:24:50 -0300

    fix: cors ports

--- src/main/java/com/proyecto/integrador/config/SecurityConfig.java
+++ src/main/java/com/proyecto/integrador/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5174"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
 
