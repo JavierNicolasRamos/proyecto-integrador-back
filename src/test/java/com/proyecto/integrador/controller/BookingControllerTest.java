c6b86d4899c2a0d3729a5b90cb0c2421da8e0339
JuanBenassi <juansebastianbenassi@gmail.com>
2023-12-01 15:27:24 -0300

    bookingcontrollertest

---
+++ src/test/java/com/proyecto/integrador/controller/BookingControllerTest.java
@@ -0,0 +1,32 @@
+package com.proyecto.integrador.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingControllerTest {
+
+    @Test
+    void listBooking() {
+    }
+
+    @Test
+    void getBooking() {
+    }
+
+    @Test
+    void createBooking() {
+    }
+
+    @Test
+    void updateBooking() {
+    }
+
+    @Test
+    void deleteBooking() {
+    }
+
+    @Test
+    void findOccupiedDates() {
+    }
+}
